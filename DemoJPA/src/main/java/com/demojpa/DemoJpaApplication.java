950ba806caec01bad2552676b36a6951de26a1fb
Paolo Sandoval <paoloadrian.sandoval@gmail.com>
2026-05-16 16:09:43 -0600

    Funciones implementadas

---
+++ DemoJPA/src/main/java/com/demojpa/DemoJpaApplication.java
@@ -0,0 +1,111 @@
+package com.demojpa;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.demojpa.models.Categoria;
+import com.demojpa.repository.ICategoriaRepository;
+
+@SpringBootApplication
+public class DemoJpaApplication implements CommandLineRunner{
+	
+	@Autowired
+	private ICategoriaRepository repoCategoria;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpaApplication.class, args);
+	}
+	
+	private void testConexion() {
+		if (repoCategoria != null)
+			System.out.println("conexion exitosa: " +repoCategoria);
+		else
+			System.out.println("conexion fallida");
+	}
+	
+	private void guardar() {
+		Categoria categoria = new Categoria();
+		categoria.setNombre("Playas");
+		categoria.setDescripcion("Viajes a las playas de este hermoso país");
+		repoCategoria.save(categoria);
+	}
+	private void buscarPorId() {
+		Optional <Categoria> optional = repoCategoria.findById(1);
+		if (optional.isPresent())
+			System.out.println(optional.get().getNombre());
+		else
+			System.out.println("Categoria no encontrada");
+	}
+	private void modificar() {
+		Optional <Categoria> optional = repoCategoria.findById(1);
+		if (optional.isPresent()) {
+			Categoria CatTemp = new Categoria();
+			CatTemp = optional.get();
+			CatTemp.setNombre("Volcanes");
+			CatTemp.setDescripcion("Largas y exigentes caminatas por los volcanes de este hermoso país");
+			repoCategoria.save(CatTemp);
+			System.out.println(optional.get());
+		}else
+			System.out.println("Categoria no encontrada");
+	}
+	private void eliminarPorId() {
+		repoCategoria.deleteById(1);
+	}
+	
+	private void cantidadCategorias() {
+		long cantidad = repoCategoria.count();
+		System.out.println("Hay categorias " + cantidad + " actualmente.");
+	}
+	private void eliminarTodo() {
+		repoCategoria.deleteAll();
+	}
+	private void encontrarPorIds() {
+		List<Integer> ids = new LinkedList<Integer>();
+		ids.add(1);
+		ids.add(2);
+		Iterable<Categoria> categoria = repoCategoria.findAllById(ids);
+		for (Categoria cat:categoria)
+			System.out.println(cat.getNombre() + "" + cat.getDescripcion());
+	}
+	private void encontrarTodos() {
+		Iterable<Categoria> categoria = repoCategoria.findAll();
+		for (Categoria cat:categoria)
+			System.out.println(cat.getNombre() + "" + cat.getDescripcion());
+	}
+	private void existeId() {
+		boolean existencia = repoCategoria.existsById(3);
+		System.out.println("¿Existe la categoria? " + existencia);
+	}
+	private List<Categoria> getCategoria(){
+		List<Categoria> lista = new LinkedList<Categoria>();
+		Categoria cat1 = new Categoria();
+		cat1.setNombre("Viaje 1");
+		cat1.setDescripcion("Descripción del viaje 1");
+		repoCategoria.save(cat1);
+		Categoria cat2 = new Categoria();
+		cat2.setNombre("Viaje 2");
+		cat2.setDescripcion("Descripción del viaje 2");
+		repoCategoria.save(cat2);
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
+	}
+	
+	private void guardartodas() {
+		List<Categoria> lista = getCategoria();
+		repoCategoria.saveAll(lista);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		existeId();
+	}
+
+}
